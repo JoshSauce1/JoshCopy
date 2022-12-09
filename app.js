96f1fcd05d74c7f16983dd48ff5a1d770f7613ae
JoshSauce1 <joshuaabreu16@gmail.com>
2022-12-09 16:04:39 -0500

    d
    d

---
+++ app.js
@@ -0,0 +1,16 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e){
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior : "smooth"
+    });
+
+  });
+});
